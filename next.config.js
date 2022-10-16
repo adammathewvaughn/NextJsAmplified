7147fd6dc4bd7c3f494ccdc454961cf47e51b0e1
adammathewvaughn <adammathewvaughn@gmail.com>
2022-10-16 12:02:09 -0500

    merged next js repos

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
 module.exports = {
   reactStrictMode: true,
 }
+=======
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
+>>>>>>> 22ac9b3 (Initial commit from Create Next App)
